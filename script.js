fe03dc49b2bf049df0973ac999d5272a578a2209
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-02-15 22:02:25 +0100

    add script for choosen option

--- script.js
+++ script.js
@@ -19,16 +19,28 @@ const choosenReward = document.querySelectorAll('.pledge button');
 console.log(choosenReward);
 let allMoney = 89914;
 let allBackers = 5007;
+let i =0;
 
 //menu display
 menu.addEventListener('click',()=>{
     nav.style.display == 'none' ? nav.style.display='flex' : nav.style.display='none';
 })
 
-//display modal section
+//display modal section + reset previous actions
 backButton.addEventListener('click',()=>{
     modalWrapper.style.display ='block';
     modal.style.display ='block';
+    checkboxes.forEach((checkbox1,idx1)=>{
+        checkbox1.checked = false;
+        modalPledges[idx1].style.outline = 'none';
+    })
+    enters.forEach((enter)=>{
+        enter.style.display = 'none';
+    })
+    inputs.forEach((input)=>{
+        input.value = '';
+    })
+    complete.style.display = 'none';
 })
 //close modal section
 modalCloseButton.addEventListener('click',()=>{
@@ -42,6 +54,8 @@ bookmark.addEventListener('click',()=>{
 //choose single pledge type
 checkboxes.forEach((checkbox,idx)=>{
     checkbox.addEventListener('click',()=>{
+        //reset complete
+        
         // reset checkboxes
         checkboxes.forEach((checkbox1,idx1)=>{
             checkbox1.checked = false;
@@ -62,7 +76,12 @@ checkboxes.forEach((checkbox,idx)=>{
         }
         //add pledge amount to all Money and +1 backer
         inputsSend[idx].addEventListener('click',()=>{
-            let pledge = parseInt(inputs[idx].value)
+            
+            i++;
+            let pledge = 0;
+            pledge = parseInt(inputs[idx].value)
+            console.log(pledge,i)
+            
             allMoney += pledge;
             money.innerHTML ='$' + allMoney.toLocaleString('en-US');
             
@@ -74,7 +93,7 @@ checkboxes.forEach((checkbox,idx)=>{
             //update progress bar
             let newValue = ((allMoney / 100000)*100).toFixed(2);
             progressBar.setAttribute('value', newValue)
-        })
+        }, {once: true})
     })
     //gotIt button truning back to main view
     gotItButton.addEventListener('click',()=>{
@@ -83,14 +102,44 @@ checkboxes.forEach((checkbox,idx)=>{
 })
  choosenReward.forEach((choosen,idx2)=>{
     choosen.addEventListener('click',()=>{
-        idx2 == 0 ? window.scrollTo(0, 350) : window.scrollTo(0,600)
+        checkboxes.forEach((checkbox1,idx1)=>{
+            checkbox1.checked = false;
+            modalPledges[idx1].style.outline = 'none';
+        })
+        enters.forEach((enter)=>{
+            enter.style.display = 'none';
+        })
+        inputs.forEach((input)=>{
+            input.value = '';
+        })
+        complete.style.display = 'none';
 
+        idx2 == 0 ? window.scrollTo(0, 350) : window.scrollTo(0,600)
         modalWrapper.style.display ='block';
         modal.style.display ='block';
-        console.log(idx2 + 1);
         checkboxes[idx2 + 1].checked = true;
         enters[idx2 + 1].style.display = 'flex';
         modalPledges[idx2 + 1].style.outline = '1px solid hsl(176, 50%, 47%)';
+        
+
+         inputsSend[idx2 + 1].addEventListener('click',()=>{
+            console.log('klikam')
+            let pledge = parseInt(inputs[idx2 + 1].value)
+            allMoney += pledge;
+            money.innerHTML ='$' + allMoney.toLocaleString('en-US');
+            
+            allBackers++;
+            backers.innerHTML = allBackers.toLocaleString('en-US');
+
+            modal.style.display ='none';
+            complete.style.display = 'flex';
+            //update progress bar
+            let newValue = ((allMoney / 100000)*100).toFixed(2);
+            progressBar.setAttribute('value', newValue);
+            console.log('koniec klika')
+        },{once:true})
+
+        
     })
  })
 
